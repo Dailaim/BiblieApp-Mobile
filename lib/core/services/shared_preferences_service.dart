46962533a48ae970efd03a0fa12f07b99e5c9533
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-19 15:02:02 -0500

    create save local service

---
+++ lib/core/services/shared_preferences_service.dart
@@ -0,0 +1,13 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  static Future<void> saveData(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
+  }
+
+  static Future<String?> getData(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+}
