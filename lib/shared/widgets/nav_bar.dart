4bd49c8ae79bfa0097158c7f65ad49a2f7cb62f9
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-22 17:14:47 -0500

    create index global app

--- lib/shared/widgets/nav_bar.dart
+++ lib/shared/widgets/nav_bar.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutterpractic/core/contexts/router_key.dart';
+import 'package:flutterpractic/core/router/router.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:line_icons/line_icons.dart';
 
-class NavBar extends StatelessWidget {
-  final GoRouter router;
-
-  const NavBar({super.key, required this.router});
+class NavBar extends HookConsumerWidget {
+  const NavBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
+    final routerKey = ref.watch(routerKeyProvider);
+    final router = ref.read(goRouterProvider);
 
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
         child: GNav(
-          gap: 8,
+          selectedIndex: routerKey,
+          onTabChange: (index) =>
+              ref.read(routerKeyProvider.notifier).state = index,
+          gap: 6,
           activeColor: theme.colorScheme.secondary,
           iconSize: 24,
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
