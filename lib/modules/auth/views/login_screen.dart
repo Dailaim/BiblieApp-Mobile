9e32e8e63c17f507dc60df5c8f90acb91bb6dda6
Leonardo Iglesias <leo-im@hotmail.com>
2023-09-22 17:13:17 -0500

    create system login

--- lib/modules/auth/views/login_screen.dart
+++ lib/modules/auth/views/login_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutterpractic/core/contexts/authContext.dart';
+import 'package:flutterpractic/core/contexts/auth_context.dart';
+import 'package:flutterpractic/core/router/router.dart';
 import 'package:flutterpractic/modules/auth/validators/email.dart';
 import 'package:flutterpractic/modules/auth/validators/password.dart';
 import 'package:go_router/go_router.dart';
@@ -11,12 +14,9 @@ class LoginScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authContext = ref.watch(authProvider);
     final formKey = useState(GlobalKey<FormState>());
     final usernameController = useTextEditingController();
     final passwordController = useTextEditingController();
-
-    final errorNotifier = useState<String?>(null);
 
     return Scaffold(
       appBar: AppBar(
@@ -38,24 +38,16 @@ class LoginScreen extends HookConsumerWidget {
                 key: formKey.value,
                 child: Column(
                   children: <Widget>[
-                    ValueListenableBuilder(
-                      valueListenable: errorNotifier,
-                      builder: (context, String? serverError, _) {
-                        return TextFormField(
-                          controller: usernameController,
-                          onChanged: (value) => errorNotifier.value = null,
-                          validator: emailValidator,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: const OutlineInputBorder(),
-                            errorText: serverError,
-                          ),
-                        );
-                      },
+                    TextFormField(
+                      controller: usernameController,
+                      validator: emailValidator,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        border: const OutlineInputBorder(),
+                      ),
                     ),
                     const SizedBox(height: 20),
                     TextFormField(
-                      onChanged: (value) => errorNotifier.value = null,
                       controller: passwordController,
                       obscureText: true,
                       validator: passwordValidator,
@@ -64,17 +56,16 @@ class LoginScreen extends HookConsumerWidget {
                         border: OutlineInputBorder(),
                       ),
                     ),
-                    const SizedBox(height: 20),
+                    SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        errorNotifier.value = null;
                         if (!formKey.value.currentState!.validate()) return;
 
-                        ref.read(authProvider.notifier).login(
-                              email: usernameController.text,
-                              password: passwordController.text,
-                              notifier: errorNotifier,
-                            );
+                        _dialogBuilder(
+                          context,
+                          email: usernameController.text,
+                          password: passwordController.text,
+                        );
                       },
                       child: const Text('Login'),
                     ),
@@ -103,6 +94,87 @@ class LoginScreen extends HookConsumerWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> _dialogBuilder(
+    BuildContext context, {
+    required String email,
+    required String password,
+  }) {
+    return showDialog<void>(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+            padding: EdgeInsets.all(16),
+            child: HookConsumer(builder: (context, ref, child) {
+              final login = ref.watch(AuthLoginProvider(
+                email: email,
+                password: password,
+              ));
+              final router = ref.watch(goRouterProvider);
+
+              return login.when(
+                data: (data) {
+                  /* // Si la autenticación es exitosa, muestra un mensaje y cierra el diálogo.
+                  Navigator.of(context).pop(); */
+                  return Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Bienvenido ${data ?? "Fatal error"}",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (data != null && data.isNotEmpty) {
+                            ref.read(authProvider.notifier).setToken(data);
+                            router.pop();
+                          }
+                          print("fagsdddddddddddddfdas");
+                          print("token: ${ref.read(authProvider).token}");
+
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Ir a Home'),
+                      ),
+                    ],
+                  );
+                },
+                error: (error, stack) {
+                  // Si hay un error, muestra el mensaje de error.
+                  return Text(
+                    "Error: ${error.toString()}",
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
+                  );
+                },
+                loading: () => Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircularProgressIndicator(), // Cambio del símbolo de carga
+                    SizedBox(height: 16),
+                    Text(
+                      "Iniciando sesión...",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ),
+        );
+      },
     );
   }
 }
