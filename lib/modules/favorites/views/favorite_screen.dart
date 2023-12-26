dfe5f126ba7a54d4c4eeb4cefa48fbc46d2ab91f
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-26 13:36:13 -0500

    Refactor FavoritesScreen widget

--- lib/modules/favorites/views/favorite_screen.dart
+++ lib/modules/favorites/views/favorite_screen.dart
@@ -3,7 +3,17 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutterpractic/core/contexts/theme_context.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'package:niku/niku.dart';
+import 'package:niku/namespace.dart' as n;
+
 final myAutoDisposeProvider = StateProvider((ref) => 0);
+
+extension ApplyToN on Niku {
+  void _add(Widget Function(Widget) builder) => $parent.add(builder);
+  void get container => _add((w) => Container(
+        child: w,
+      ));
+}
 
 class FavoritesScreen extends HookConsumerWidget {
   FavoritesScreen({Key? key}) : super(key: key);
@@ -12,50 +22,44 @@ class FavoritesScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final count = useState<int>(0);
 
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Count: ${count.value}"),
-              OutlinedButton(
-                onPressed: () => count.value++,
-                child: const Text('Increment'),
-              ),
-              HookConsumer(builder: (context, ref, child) {
-                final countP = ref.watch(myAutoDisposeProvider);
-                final themeApp = ref.watch(colorThemeProvider);
+    final countP = ref.watch(myAutoDisposeProvider);
+    final themeApp = ref.watch(colorThemeProvider);
 
-                return Column(
-                  children: [
-                    Text("Count: ${countP}"),
-                    OutlinedButton(
-                      onPressed: () =>
-                          ref.watch(myAutoDisposeProvider.notifier).state++,
-                      child: const Text('Increment'),
-                    ),
-                    OutlinedButton(
-                      onPressed: () => {
-                        ref
-                            .read(colorThemeProvider.notifier)
-                            .toggleTheme(themeApp + 1 < 9 ? themeApp + 1 : 0)
-                      },
-                      child: const Text('setTheme'),
-                    ),
-                    OutlinedButton(
-                      onPressed: () =>
-                          {ref.read(darkThemeProvider.notifier).toggleDark()},
-                      child: const Text('setDark'),
-                    ),
-                  ],
-                );
-              }),
-            ],
-          ),
+    return n.Column(
+      [
+        Text("Count: ${count.value}"),
+        OutlinedButton(
+          onPressed: () => count.value++,
+          child: const Text('Increment'),
         ),
-      ),
-    );
+        n.Column(
+          [
+            "Count: ${countP}".n,
+            OutlinedButton(
+              onPressed: () =>
+                  ref.watch(myAutoDisposeProvider.notifier).state++,
+              child: 'Increment'.n,
+            ),
+            OutlinedButton(
+              onPressed: () => {
+                ref
+                    .read(colorThemeProvider.notifier)
+                    .toggleTheme(themeApp + 1 < 9 ? themeApp + 1 : 0)
+              },
+              child: 'setTheme'.n,
+            ),
+            OutlinedButton(
+              onPressed: () =>
+                  {ref.read(darkThemeProvider.notifier).toggleDark()},
+              child: 'setDark'.n,
+            ),
+          ],
+        ),
+      ],
+    )
+      ..mainAxisAlignment = MainAxisAlignment.center
+      ..center
+      ..padding = const EdgeInsets.all(16.0)
+      ..useParent((w) => w..container);
   }
 }
