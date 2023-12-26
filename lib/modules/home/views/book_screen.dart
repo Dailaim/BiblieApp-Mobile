a2f4ca6fe4df998507a0aaf63ffa0a824cdfc03b
Leonardo Iglesias <laiglesias.min@gmail.com>
2023-12-26 13:36:09 -0500

    Refactor book screen layout and add card styling

--- lib/modules/home/views/book_screen.dart
+++ lib/modules/home/views/book_screen.dart
@@ -4,63 +4,60 @@ import 'package:flutterpractic/core/utils/dynamic_columns.dart';
 import 'package:flutterpractic/modules/home/context/book_context.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:niku/niku.dart';
+import 'package:niku/namespace.dart' as n;
+
+extension ApplyToUseParent on Niku {
+  void _add(Widget Function(Widget) builder) => $parent.add(builder);
+  void get card => _add((w) => Card(
+        child: w,
+      ));
+}
 
 class BookScreen extends HookConsumerWidget {
   const BookScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final bibleContext = ref.watch(bibleProvider);
+    final version = ref.watch(bibleVersionProvider);
+    final book = ref.watch(bibleBookProvider);
     final columns = useDynamicColumns(120);
 
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            Text(bibleContext.book!.name),
+        title: n.Row(
+          [
+            book!.name.n,
             const Spacer(),
-            Text(bibleContext.version!.short ?? bibleContext.version!.name),
+            Text(version.short ?? version.name),
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: columns,
-            crossAxisSpacing: 1.0,
-            mainAxisSpacing: 1.0,
-          ),
-          itemCount: bibleContext.book!.chapters + 15,
-          itemBuilder: (context, index) {
-            if (index >= bibleContext.book!.chapters) {
-              return const SizedBox.shrink();
-            }
-            return Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Card(
-                child: InkWell(
-                  onTap: () {
-                    ref.read(bibleProvider.notifier).state =
-                        ref.read(bibleProvider.notifier).state.copyWith(
-                              chapter: index + 1,
-                            );
-                    context.go('/book/chapter');
-                  },
-                  child: Center(
-                    child: Text(
-                      (index + 1).toString(),
-                      style: const TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          },
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: columns,
+          crossAxisSpacing: 1.0,
+          mainAxisSpacing: 1.0,
         ),
-      ),
+        itemCount: book.chapters + 15,
+        itemBuilder: (context, index) {
+          if (index >= book.chapters) {
+            return const SizedBox.shrink();
+          }
+          return InkWell(
+            onTap: () {
+              ref.read(bibleChapterProvider.notifier).changeChapter(index + 1);
+              context.go('/book/chapter');
+            },
+            child: Center(
+              child: (index + 1).toString().n..fontSize = 16,
+            ),
+          ).niku
+            ..card
+            ..padding = const EdgeInsets.all(2.0);
+        },
+      ).niku
+        ..padding = const EdgeInsets.all(8.0),
     );
   }
 }
